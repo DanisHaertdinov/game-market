d85ddf4fb460d8b695552e544d5f973f9d1df871
Danis <danis.haertdinov@gmail.com>
2021-04-05 15:17:00 +0400

    add game-market files

---
+++ src/components/PurchaseForm/PurchaseForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BuyStatus, Purchase} from '../../data/types';
+
+interface PurchaseFormProps {
+    value: Purchase;
+    onChange: (value: Purchase) => void;
+    buyStatus?: BuyStatus;
+}
+
+export function PurchaseForm({value, buyStatus, onChange}: PurchaseFormProps) {
+    return <form>
+
+    </form>;
+}
