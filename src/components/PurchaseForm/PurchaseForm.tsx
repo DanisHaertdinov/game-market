2bf9181ac759c01d4dbedd948efc4c10cd58d39d
Danis <danis.haertdinov@gmail.com>
2021-04-08 18:25:31 +0400

    passed all test cases

--- src/components/PurchaseForm/PurchaseForm.tsx
+++ src/components/PurchaseForm/PurchaseForm.tsx
@@ -1,6 +1,8 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {BuyStatus, Purchase, UserInfo} from '../../data/types';
-import {APIContext, CurrentUserContext} from "../Context";
+import {APIContext, CurrentUserContext} from '../Context';
+import {Checkbox} from "../Common/Checkbox";
+import {TextInput} from "../Common/TextInput";
 
 interface PurchaseFormProps {
     value: Purchase;
@@ -8,93 +10,146 @@ interface PurchaseFormProps {
     buyStatus?: BuyStatus;
 }
 
+interface UserDisclaimer {
+    id: number;
+    type: number;
+}
+
+enum AgeDisclaimerTypes {
+    incorrectAge,
+    noAge,
+    ok
+}
+
+const AgeDisclaimerTexts = new Map<number, string>([
+    [AgeDisclaimerTypes.incorrectAge, 'The person is not allowed to get the game due to age restriction'],
+    [AgeDisclaimerTypes.noAge, 'Cannot be selected unless user\'s age is specified, because the game has age restriction']
+]);
+
 export function PurchaseForm({value, buyStatus, onChange}: PurchaseFormProps) {
     const api = useContext(APIContext);
     const currentUser = useContext(CurrentUserContext);
-    const {game, emails=[], userIds=[]} = value
+    const {game, emails = [], userIds = [], acknowledgeInviteAge = false, acknowledgeInvite = false} = value;
 
     const [users, setUsers] = useState<UserInfo[]>([]);
     const [isInviteFormShown, setInviteFormShown] = useState(false);
+    const [userDisclaimer, setUserDisclaimer] = useState<UserDisclaimer>()
 
     useEffect( () => {
         if (currentUser !== undefined) {
             api.users.fetchFriends({userId: currentUser[`id`]}).then((users) => {
-                setUsers([currentUser, ...users]);
+                const sortedUsers = users.sort((userA, userB) => userA.name.localeCompare(userB.name))
+                setUsers([currentUser, ...sortedUsers]);
             });
         }
-    }, [currentUser]);
+    }, [currentUser, api]);
 
-    const handleUserCheckboxChange = (evt: React.ChangeEvent<HTMLInputElement>, user: UserInfo) => {
-        const isChecked = evt.target.checked;
-        let ids = [...userIds]
-        if (isChecked) {
-            ids.push(user.id)
-        }
-        else {
-            const idIndex = ids.findIndex((id) => id === user.id)
-            ids.splice(idIndex, 1);
+    const validateUserAge = (minAge:number | undefined, userAge:number | undefined):{isValid: boolean, type: number} => {
+        if (typeof minAge === `undefined`) {
+            return {isValid: true, type: AgeDisclaimerTypes.ok};
         }
 
-        onChange({...value, userIds: ids})
-    }
+        if (typeof userAge === `undefined`) {
+            return {isValid: false, type: AgeDisclaimerTypes.noAge};
+        }
 
-    const handleInviteCheckboxChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-        const isChecked = evt.target.checked;
+        if (userAge < minAge) {
+            return {isValid: false, type: AgeDisclaimerTypes.incorrectAge};
+        }
 
+        return {isValid: true, type: AgeDisclaimerTypes.ok};
+    };
+
+    const handleUserCheckboxChange = (isChecked:boolean, user: UserInfo) => {
+        const {isValid, type} = validateUserAge(game.restrictions?.minAge, user.age);
+
+        if (!isValid) {
+            setUserDisclaimer({id: user.id, type: type});
+            return
+        }
+
+        const idIndex = userIds.findIndex((id) => id === user.id);
+        const ids = isChecked ? [...userIds, user.id] : [...userIds].splice(idIndex, 1);
+
+        onChange({...value, userIds: ids});
+    };
+
+    const handleInviteCheckboxChange = (isChecked:boolean) => {
         if (!isChecked) {
-            onChange({...value, emails: [], acknowledgeInvite: false, acknowledgeInviteAge: false})
+            onChange({...value, emails: [], acknowledgeInvite: false, acknowledgeInviteAge: false});
         }
 
         setInviteFormShown(isChecked);
     };
 
-    const handleInviteInputChange = (evt: React.ChangeEvent<HTMLInputElement>, id:number) => {
-        const email = evt.target.value;
+    const handleInviteInputChange = (email:string, id:number) => {
         const newEmails = [...emails];
 
         newEmails[id] = email;
 
-        onChange({...value, emails: newEmails})
+        onChange({...value, emails: newEmails});
     };
 
-    const handleAcknowledgeInviteChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-        const isChecked = evt.target.checked;
-        const checkboxName = evt.target.name;
-
+    const handleAcknowledgeInviteChange = (isChecked:boolean, checkboxName:string) => {
         onChange({...value, [checkboxName]: isChecked});
     };
 
     return (
         <form>
             {users.map((user,index) => (
-                <label key={user.id} data-testid={`user${user.id}Label`}>
-                    <input onChange={(evt) => handleUserCheckboxChange(evt, user)} type="checkbox" data-testid={`user${user.id}`}/>
-                    {user.name}{index === 0 && (` (me)`)}
-                </label>
+                <React.Fragment key={`fragment${user.id}`}>
+                    {(userDisclaimer?.id === user.id) && <span key={`disclaimer${user.id}`} data-testid={`user${user.id}${AgeDisclaimerTypes[userDisclaimer.type]}`}>{AgeDisclaimerTexts.get(userDisclaimer.type)}</span>}
+                    <Checkbox
+                        key={user.id}
+                        testId={`user${user.id}`}
+                        disabled={buyStatus === BuyStatus.inProgress}
+                        onChange={(isChecked) => handleUserCheckboxChange(isChecked, user)}
+                        checked={userIds.includes(user.id)}>
+                        {user.name}{index === 0 && (` (me)`)}
+                    </Checkbox>
+                </React.Fragment>
             ))}
-            <label>
-                <input type="checkbox" checked={isInviteFormShown} onChange={handleInviteCheckboxChange} data-testid={`showInvite`}/>
+            <Checkbox
+                checked={isInviteFormShown}
+                onChange={handleInviteCheckboxChange}
+                testId={'showInvite'}
+                disabled={buyStatus === BuyStatus.inProgress}
+            >
                 Invite friends
-            </label>
+            </Checkbox>
             {isInviteFormShown
             &&
             <div data-testid={`invite`}>
                 {[...emails, ``].map((email,index) => (
-                    <label key={`email${index}`}>
-                        <input type="email" value={email} data-testid={`email${index}`} onChange={(evt) => handleInviteInputChange(evt, index)}/>
-                    </label>
+                    <TextInput
+                        key={`email${index}`}
+                        value={email}
+                        disabled={buyStatus === BuyStatus.inProgress}
+                        testId={`email${index}`}
+                        onChange={(value) => handleInviteInputChange(value, index)}
+                    />
                 ))}
-                <label>
-                    <input type="checkbox" name="acknowledgeInvite" data-testid="acknowledgeInvite" required onChange={handleAcknowledgeInviteChange}/>
+                <Checkbox
+                    onChange={(isChecked) => handleAcknowledgeInviteChange(isChecked, 'acknowledgeInvite')}
+                    testId={'acknowledgeInvite'}
+                    disabled={buyStatus === BuyStatus.inProgress}
+                    checked={acknowledgeInvite}
+                >
                     I acknowledge that Game Market invitation emails will be sent to specified emails.
                     The game will become available to the person only onсe the registration in the Game Market is completed.
-                </label>
-                {game.restrictions
+                </Checkbox>
+                {
+                    game.restrictions?.minAge
                     &&
-                    <label>
-                        <input type="checkbox" name="acknowledgeInviteAge" data-testid="acknowledgeInviteAge" required onChange={handleAcknowledgeInviteChange}/>
+                    <Checkbox
+                        onChange={(isChecked) => handleAcknowledgeInviteChange(isChecked, 'acknowledgeInviteAge')}
+                        testId={'acknowledgeInviteAge'}
+                        disabled={buyStatus === BuyStatus.inProgress}
+                        checked={acknowledgeInviteAge}
+                    >
                         I acknowledge that the game has age restriction and might be unavailable if a person is under required age.
-                    </label>}
+                    </Checkbox>
+                }
             </div>}
         </form>
     );
